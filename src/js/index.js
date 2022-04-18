5287526789c729eb3a1e14f236b908794b5838da
dimas <89324713054@mail.ru>
2022-04-18 22:38:20 +0500

    Есть дорога

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,5 @@ const {app, BrowserWindow} = require('electron')
 function createWindow () {
     window = new BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration:true, contextIsolation:false}})
     window.loadFile('src/html/index.html')
-    // window2 = new BrowserWindow({width: 1000, height: 600, webPreferences: {nodeIntegration:true, contextIsolation:false}})
-    // window2.loadFile('src/html/image.html')
 }
 app.on('ready', createWindow);
