55de71ec4007c485ce3ed5c397b5b5f61974d3ea
dimas <89324713054@mail.ru>
2022-06-09 01:39:11 +0500

    index added

--- src/js/index.js
+++ src/js/index.js
@@ -6,35 +6,35 @@
 //         Для Windows
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-// const {ipcMain} = require("electron");
-// const {app, BrowserWindow} = require("electron");
-//
-// const path = require("path");
-// const database = require("../../database/database_module");
-//
-// function main() {
-//     window = new BrowserWindow({
-//         width: 800,
-//         height: 600,
-//         minHeight: 600,
-//         minWidth: 800,
-//         webPreferences: {
-//             nodeIntegration: true,
-//             contextIsolation: false,
-//             preload: path.join(process.cwd(), "src/js/renderer/preload.js")
-//         },
-//         icon: path.join(process.cwd(), "storage/favicon.png")
-//     });
-//     window.loadFile(path.join(process.cwd(), "src/html/index.html"));
-//     window.removeMenu(true)
-//
-//     runEventsListeners = require(path.join(
-//         process.cwd(),
-//         "src/js/mainEvents.js"
-//     ));
-//     runEventsListeners(database, ipcMain);
-// }
-// database.Init(main);
+const {ipcMain} = require("electron");
+const {app, BrowserWindow} = require("electron");
+
+const path = require("path");
+const database = require("../../database/database_module");
+
+function main() {
+    window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minHeight: 600,
+        minWidth: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(process.cwd(), "src/js/renderer/preload.js")
+        },
+        icon: path.join(process.cwd(), "storage/favicon.png")
+    });
+    window.loadFile(path.join(process.cwd(), "src/html/index.html"));
+    window.removeMenu(true)
+
+    runEventsListeners = require(path.join(
+        process.cwd(),
+        "src/js/mainEvents.js"
+    ));
+    runEventsListeners(database, ipcMain);
+}
+database.Init(main);
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //         Для Linux
