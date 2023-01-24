b8bfac84dbe055e651cce5ad3cedd9cffa5064a1
dimas <89324713054@mail.ru>
2023-01-24 11:01:50 +0500

    added CustomLineMesh.js to implement line meshes into Vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <Renderer ref="rendererC" antialias resize="window">
     <Camera ref="camera" :fov="75" :aspect="width / height" :position="{ y: 2, z: 4 }" :far="1000" :lookAt="{ y: 1.5, z: 0 }"/>
     <Scene ref="scene" :background="'white'">
-      <Box :size="1" ref="meshC" :position="{x: 1, y: 0, z: -1}">
-        <BasicMaterial color="green"/>
-      </Box>
+      <Group>
+        <Box v-for="i in 2" :size="1" ref="meshC" :position="{x: 0, y: 0, z: -i+1}">
+          <BasicMaterial color="green"/>
+        </Box>
+        <LineGeometry v-for="i in 2" ref="lines" v-bind="{x: 0, y: 0, z: -i+1}"/>
+      </Group>
     </Scene>
   </Renderer>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import { Box, Camera, BasicMaterial, Renderer, Scene } from 'troisjs'
-import * as THREE from 'three'
+import LineGeometry from './CustomLineMesh'
 
 let parentElement = document.getElementById("app");
 let width = parentElement.offsetWidth;
@@ -22,14 +25,6 @@ const rendererC = ref()
 const meshC = ref()
 const cameraC = ref()
 const scene = ref()
-</script>
-
-<script>
-export default {
-  mounted() {
-    this.$refs.scene.scene.fog = new THREE.Fog(0xFFFFFF, 10, 16)
-  }
-}
 </script>
 
 <style>
---
+++ src/CustomLineMesh.js
@@ -0,0 +1,32 @@
+import {BufferGeometry, Fog, Line, LineBasicMaterial, Vector3} from 'three'
+
+export default {
+    props: {
+        x: { type: Number, default: 0 },
+        y: { type: Number, default: 0 },
+        z: { type: Number, default: 0 }
+    },
+    methods: {
+        // TODO: Need to refactor
+        createLine() {
+           return createLine({ x: this.x, y: this.y, z: this.z }) // если передавать параметры напрямую, то они будут undefined
+        }
+    },
+    mounted() {
+        let scene = this.$root.$refs.scene.scene
+
+        scene.fog = new Fog(0xFFFFFF, 10, 16)
+        this.$root.$refs.lines.forEach(function(line) {
+            scene.add(line.createLine())
+        });
+    }
+}
+
+function createLine({ x = 0, y = 0, z = 0 }) {
+    let points = [];
+    points.push( new Vector3( x - 0.5, y + 0.51, z ) );
+    points.push( new Vector3( x + 0.5, y + 0.51, z ) );
+    const geometry = new BufferGeometry().setFromPoints(points);
+    const material = new LineBasicMaterial( { color: 0x000000 } );
+    return new Line(geometry, material)
+}
